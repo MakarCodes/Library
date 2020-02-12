e5d22c38537c219ebfcdcd4dce67aa757f05fe25
MakarCodes <jarek.strupek@wp.pl>
2020-02-12 19:05:08 +0100

    added filtering funcionality and local storage

--- library.js
+++ library.js
@@ -6,8 +6,39 @@ let booksContainer = document.querySelector('.books-container');
 const clearButton = document.querySelector('#cancel');
 const readCheckbox = document.querySelector('#read');
 const deleteButton = document.querySelector('.delete');
+const search = document.querySelector('#search');
+let userText = '';
 
+class Store {
+    static getBooks() {
+        let books;
+        if(localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
 
+    static addBook(book) {
+        const books = Store.getBooks();
+
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(id) {
+        const books = Store.getBooks();
+
+        books.forEach((book, index) => {
+            if(book.id == id) {
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
 
 //constructor function - creating new book
 function Book(id, title, author, pages, year, read) {
@@ -23,7 +54,8 @@ const bookOne = new Book(0, 'Lord of the Rings', 'J.R.R. Tolkien', 2564, 1954, t
 const bookTwo = new Book(1, 'The Shadow of the wind', 'Carlos Ruiz Zafón', 598, 2001, false);
 const bookThree = new Book(2, 'Mężczyźni, którzy nienawidzą kobiet', 'Stieg Larsson', 456, 2005, true);
 
-myLibrary.push(bookOne, bookTwo, bookThree);
+// myLibrary.push(bookOne, bookTwo, bookThree);
+myLibrary = Store.getBooks();
 initRender();
 
 // event listeners
@@ -35,8 +67,10 @@ booksContainer.addEventListener('click', e => {
         myLibrary =  myLibrary.filter(book => {
             return book.id != currentId;
         })
+        Store.removeBook(currentId);
         e.target.parentElement.remove();
     }
+    console.log(myLibrary)
 })
 
 addBookButton.addEventListener('click', () => {
@@ -49,6 +83,8 @@ form.addEventListener('submit', (e) => {
     formDisplay.style.display = 'none';
     render();
     clearForm();
+    bookFilteringByAuthor(userText);
+    // bookFilteringByTitle(userText);
 })
 
 form.addEventListener('click', e => {
@@ -72,26 +108,31 @@ function initRender() {
         html += generateHTML(book);
     })
     booksContainer.innerHTML = html;
-    addedBook = document.querySelectorAll('.display-added-book');
+}
+
+function filterRender(bookArray) {
+    let html = '';
+    bookArray.forEach((book) => {
+        html += generateHTML(book);
+    });
+    booksContainer.innerHTML = html;
 }
 
 function render(){
     booksContainer.innerHTML += generateHTML(myLibrary[myLibrary.length-1]); 
-    addedBook = document.querySelectorAll('.display-added-book');
 }
 
 booksContainer.addEventListener('click', e => {
-    if(e.target.id == 'read-status'){
-        let index = e.target.parentElement.getAttribute('data-index');
-        const currentReadStatus = e.target.checked;
+    let index = e.target.parentElement.getAttribute('data-index');
+    const currentReadStatus = e.target.checked;
+    if(e.target.id == `read-status${index}`){
         changeReadStatusInArray(index, currentReadStatus);
     }
 })
 
 function changeReadStatusInArray(index, currentReadStatus){
-    console.log('before', myLibrary[index]);
-    myLibrary[index].read = !myLibrary[index].read;
-    console.log('after', myLibrary[index]);
+    myLibrary[index].read = currentReadStatus;
+    document.getElementById(`read-status${index}`).checked = currentReadStatus;
 }
 
 
@@ -105,11 +146,11 @@ function generateHTML({title, author, pages, year, read, id}){
     html = `
     <div class="display-added-book" data-index="${id}">
         <p>Title: <span>${title}</span></p>
-        <p>Author: <span>${author}</span></p>
+        <p class="author">Author: <span>${author}</span></p>
         <p>Pages: <span>${pages}</span></p>
         <p>Year of publication: <span>${year}</span></p>
-        <label for="read-status" id="read-status">Read status:</label>
-        <input type="checkbox" name="read-status" id="read-status" ${checkStatus}>
+        <label for="read-status${id}">Read status:</label>
+        <input type="checkbox" name="read-status" id="read-status${id}" ${checkStatus}>
         <button class="delete">Delete</button>
     </div>
     `;
@@ -122,14 +163,38 @@ function addBookToLibrary(){
     const authorName = document.querySelector('#author');
     const numberOfPages = document.querySelector('#pages');
     const yearOfPublication = document.querySelector('#year');
-    const readStatus = document.querySelector('#read');
     const newBookId = myLibrary.length;
     let book = new Book (newBookId, titleName.value, authorName.value, numberOfPages.value, yearOfPublication.value, form.read.checked);
     myLibrary.push(book);
+    Store.addBook(book);
 }
 
 
+function bookFilteringByAuthor(userText) {
+    let filteredArrayByAuthors = myLibrary.filter(singleBook => 
+        singleBook.author.toLowerCase().includes(userText) || 
+        singleBook.title.toLowerCase().includes(userText));
+    filterRender(filteredArrayByAuthors);
+    
+}
+
+function bookFilteringByTitle(userText) {
+    let filteredArrayByTitles = myLibrary.filter(book => book.title.toLowerCase().includes(userText));
+    filterRender(filteredArrayByTitles);
+}
+
+search.addEventListener('keyup', e => {
+    userText = search.value.trim().toLowerCase(); 
+    bookFilteringByAuthor(userText);
+    // bookFilteringByTitle(userText);
+    
+});
 
 
+    // jakaś funkcja filtrująca z parametrem propertiesArray i iteruje po tej tablicy 
+    //w poszukiwaniu "matchów" w zadeklarowanych propertiesach.
 
+
+   // 1. Filtrowanie warunkowe: filtrujemy tablice wszystkich książek i zwracamy rekord jeśli jest "match" w property author LUB proerty title
+    // 2. Odfiltrowanie powtarzających się elementów z tablicy (Lodash (_.uniq))
 
